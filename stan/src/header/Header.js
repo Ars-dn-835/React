bebd3b36bb36ae509a9e663211a07f9df6aa7006
Arsen Deneka <99550724+Ars-dn-835@users.noreply.github.com>
2022-09-23 14:02:03 +0300

    Add files via upload

---
+++ stan/src/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "./Menu";
+import './header.css';
+
+function Header() {
+  return (
+    <header className="Header">
+        <Menu col="yellow" fs = "46px"/>
+     хедерочик
+    </header>
+  );
+}
+
+export default Header;
